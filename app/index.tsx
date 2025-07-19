4b35b15965c52651c4c3f476e98c19d02607f021
Curtis Sila <curtissila4@gmail.com>
2025-07-19 12:30:46 +0300

    Home page v1

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
 import {SafeAreaView} from "react-native-safe-area-context";
-import {FlatList, Pressable, View, Text} from "react-native";
-import {offers} from "@/constants";
+import {FlatList, Pressable, View, Text, Image} from "react-native";
+import {images, offers} from "@/constants";
+import {Fragment} from "react";
+import cn from 'clsx'
 
 export default function Index() {
   return (
@@ -8,10 +10,30 @@ export default function Index() {
          <FlatList
             data={offers}
             renderItem={({item, index}) => {
+                const isEven = index % 2 === 0;
+
                 return (
                     <View>
-                        <Pressable className="bg-amber-600">
-                            <Text>{item.title}</Text>
+                        <Pressable className={cn("offer-card", isEven ? 'flex-row-reverse' : 'flex-row')}
+                                   style={{ backgroundColor: item.color}}>
+                            {({ pressed }) => (
+                                <Fragment>
+                                    <View className={"h-full w-1/2"}>
+                                        <Image source={item.image} className={"size-full"} resizeMode={"contain"} />
+                                    </View>
+                                    <View className={"offer-card__info"}>
+                                        <Text className="h1-bold text-white leading-tight">
+                                            {item.title}
+                                        </Text>
+                                        <Image
+                                        source={images.arrowRight}
+                                        className="size-10"
+                                        resizeMode="contain"
+                                        tintColor="ffffff"
+                                        />
+                                    </View>
+                                </Fragment>
+                            )}
                         </Pressable>
                     </View>
                 )
