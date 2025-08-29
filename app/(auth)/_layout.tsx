9243a0f55f315f08ce508df9d3fec956498f2a93
muasyasila <curtissila4@gmail.com>
2025-08-29 15:38:38 +0300

    Updates

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,9 @@
+import {View, Text} from "react-native";
+import React from 'react'
+export default function _layout() {
+    return (
+        <View>
+            <Text>Auth Layout</Text>
+        </View>
+    )
+}
